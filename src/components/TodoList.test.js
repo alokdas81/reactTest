e888226a194c4d0110744852d3352e4a0290fbc5
akashOGMA <akash.golui@ogmaconceptions.com>
2019-06-12 19:59:10 +0530

    test commit

--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -1,9 +1,20 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import TodoList from './TodoList'
+// import React from 'react'
+// import renderer from 'react-test-renderer'
+// import TodoList from './TodoList'
 
-test('TodoList renders correctly', ()=>{
-    const component = renderer.create( <TodoList />)
-    const tree = component.toJSON()
-    expect(tree).toMatchSnapshot()
-})
+// test('TodoList renders correctly', ()=>{
+//     const component = renderer.create( <TodoList />)
+//     const tree = component.toJSON()
+//     expect(tree).toMatchSnapshot()
+// })
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoList from './TodoList';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<TodoList />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
